2451282643d40188a26fe80de3f5292d59b91adb
Johny Prieto <johnyprieto001@gmail.com>
2022-12-20 16:47:43 -0500

    add config to use new api

--- config/services.php
+++ config/services.php
@@ -31,4 +31,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'conectados_web' => [
+        'uri' => env('CONECTADOS_WEB_API'),
+        'token' => env('CONECTADOS_WEB_TOKEN')
+    ],
+
 ];
